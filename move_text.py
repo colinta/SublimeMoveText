7a96c38206221eef2e5f84eddf1fd8d828aad50d
Colin Thomas-Arnold <colinta@mac.com>
2012-01-01 09:54:28 -0700

    fixed moving multiple lines and some refactors (use begin() and end())

--- move_text.py
+++ move_text.py
@@ -8,24 +8,22 @@ def move_text_horiz(view, edit, direction):
             continue
 
         orig_region = region
-        if region.a > region.b:
-            region = Region(region.b, region.a)
+        sel_region = Region(region.begin() + direction, region.end() + direction)
 
-        sel_region = Region(region.a + direction, region.b + direction)
         if sel_region.a < 0 or sel_region.b >= view.size():
             continue
 
         if direction < 0:
-            dest_region = Region(region.a + direction, region.b)
-            substitute = view.substr(region) + view.substr(Region(region.a + direction, region.a))
+            dest_region = Region(region.begin() + direction, region.end())
+            move_text = view.substr(region) + view.substr(Region(region.begin() + direction, region.begin()))
         else:
-            dest_region = Region(region.a, region.b + direction)
-            substitute = view.substr(Region(region.b, region.b + direction)) + view.substr(region)
+            dest_region = Region(region.begin(), region.end() + direction)
+            move_text = view.substr(Region(region.end(), region.end() + direction)) + view.substr(region)
 
         # Remove selection from RegionSet
         view.sel().subtract(orig_region)
         # Replace the selection with transformed text
-        view.replace(edit, dest_region, substitute)
+        view.replace(edit, dest_region, move_text)
         # Add the new selection
         view.sel().add(sel_region)
 
@@ -44,58 +42,68 @@ class MoveTextRightCommand(TextCommand):
         self.view.end_edit(e)
 
 
-def move_text_vert(view, edit, direction):
-    for region in view.sel():
+class MoveTextVertCommand(TextCommand):
+    def __init__(self, view):
+        super(MoveTextVertCommand, self).__init__(view)
+        view.move_text_vert_column = None
+
+    def move_text_vert(self, region, edit, direction):
         if region.empty():
-            continue
+            return
 
         orig_region = region
-        if region.a > region.b:
-            region = Region(region.b, region.a)
+        move_text = self.view.substr(region)
+        self.view.sel().subtract(orig_region)
+        self.view.replace(edit, region, '')
 
         # calculate number of characters to the left
         # of the *last* row
-        if direction < 0:
-            (row, col) = view.rowcol(region.a)
+        if direction > 1:
+            row, col = self.view.rowcol(region.begin())
         else:
-            (row, col) = view.rowcol(region.b)
+            row, col = self.view.rowcol(region.begin())
+
+        # if the last command was a vertical move, use that column
+        # the column is stored on the view - each command has its own instance,
+        # and we don't want two buffers to modify the same object (e.g. MoveTextVertCommand)
+        (cmd, _, _) = self.view.command_history(0, True)
+        if cmd != 'move_text_up' and cmd != 'move_text_down':
+            self.view.move_text_vert_column = col
+        else:
+            col = self.view.move_text_vert_column
+
         dest_row = row + direction
 
-        if direction < 0 and dest_row < 0 or \
-           direction > 0 and dest_row >= view.rowcol(view.size())[0]:
-            continue
+        if dest_row < 0 or dest_row > self.view.rowcol(self.view.size())[0]:
+            return
+
         # starting at the destination row, count off "col" characters
-        point = view.text_point(dest_row, 0)
-        #
+        dest_point = self.view.text_point(dest_row, 0)
+
         # it's possible that there aren't enough characters in the destination row,
         # so try and advance the column until we end up on the wrong row
-        while view.rowcol(point + 1)[0] == dest_row and view.rowcol(point + 1)[1] <= col:
-            point += 1
-        print view.rowcol(point)
+        while self.view.rowcol(dest_point + 1)[0] == dest_row and self.view.rowcol(dest_point + 1)[1] <= col:
+            dest_point += 1
 
-        # status_message(str(point))
+        insert_region = Region(dest_point, dest_point)
+        sel_region = Region(dest_point, dest_point + len(move_text))
 
-        substitute = view.substr(region)
-        if direction > 0:
-            point -= len(substitute)
-        insert_region = Region(point, point)
-        sel_region = Region(point, point + len(substitute))
-
-        view.sel().subtract(orig_region)
-        view.replace(edit, region, '')
-        view.replace(edit, insert_region, substitute)
-        view.sel().add(sel_region)
+        self.view.replace(edit, insert_region, move_text)
+        self.view.sel().add(sel_region)
+        self.view.show(sel_region)
 
 
-class MoveTextUpCommand(TextCommand):
+class MoveTextUpCommand(MoveTextVertCommand):
     def run(self, edit):
         e = self.view.begin_edit('move_text_vert')
-        move_text_vert(self.view, edit, -1)
+        for region in self.view.sel():
+            self.move_text_vert(region, edit, -1)
         self.view.end_edit(e)
 
 
-class MoveTextDownCommand(TextCommand):
+class MoveTextDownCommand(MoveTextVertCommand):
     def run(self, edit):
         e = self.view.begin_edit('move_text_vert')
-        move_text_vert(self.view, edit, 1)
+        for region in self.view.sel():
+            self.move_text_vert(region, edit, 1)
         self.view.end_edit(e)
